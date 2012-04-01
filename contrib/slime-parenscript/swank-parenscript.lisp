cf7426c16cac0c86b8b2906c98d24e196042cb0d
Bart Botta <00003b@gmail.com>
2012-04-01 04:18:31 -0500

    swank expects contribs to call PROVIDE now

--- contrib/slime-parenscript/swank-parenscript.lisp
+++ contrib/slime-parenscript/swank-parenscript.lisp
@@ -1,3 +1,5 @@
 ;; this file is loaded up in Common Lisp via slime-parenscript-init
 ;; we simply asdf-load the slime-parenscript system
 (asdf:operate 'asdf:load-op :swank-parenscript)
+;; and tell swank it loaded
+(provide :swank-parenscript)
